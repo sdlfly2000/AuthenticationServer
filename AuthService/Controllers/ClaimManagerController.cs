6eeb458057295078a6fefb596cdaba6a7f159e00
Jay Shi <25982097@qq.com>
2023-10-23 22:25:33 +0800

    Add Claim

--- AuthService/Controllers/ClaimManagerController.cs
+++ AuthService/Controllers/ClaimManagerController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuthService.Models;
+using Infra.Database.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace AuthService.Controllers
 {
@@ -6,5 +10,30 @@ namespace AuthService.Controllers
     [Route("[controller]/[action]")]
     public class ClaimManagerController : ControllerBase
     {
+        private readonly RoleManager<RoleEntity> _roleManager;
+        private readonly UserManager<UserEntity> _userManager;
+
+        public ClaimManagerController(
+            RoleManager<RoleEntity> roleManager, 
+            UserManager<UserEntity> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUserClaim([FromBody] AddUserClaimRequest request)
+        {
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if (user == null) { return Problem("User does not exist."); }
+
+            var claimName = new Claim(ClaimTypes.Name, request.Claim, ClaimValueTypes.String);
+            var claimNameIdentifiers = new Claim(ClaimTypes.NameIdentifier, user.Id.ToString(), ClaimValueTypes.String);
+
+            var result = await _userManager.AddClaimsAsync(user, new[] { claimName, claimNameIdentifiers});
+            if (!result.Succeeded) { return Problem("Failed to Add Claim"); }
+
+            return Ok();
+        }
     }
 }
---
+++ AuthService/Models/AddUserClaimRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthService.Models
+{
+    public class AddUserClaimRequest
+    {
+        public string UserName { get; set; }
+        public string Claim { get; set; }
+    }
+}
