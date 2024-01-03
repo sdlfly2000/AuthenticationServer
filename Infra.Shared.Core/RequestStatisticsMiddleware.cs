c0d3772fa1e89ffb42111e1cc73bb4db9c1ae54b
Jay Shi <25982097@qq.com>
2024-01-03 10:57:59 +0800

    Initial RequestStatistics Middleware

---
+++ Infra.Shared.Core/RequestStatisticsMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Common.Core.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+
+namespace Infra.Shared.Core
+{
+    [ServiceLocate(default)]
+    public class RequestStatisticsMiddleware : IMiddleware
+    {
+        private readonly ILogger<RequestStatisticsMiddleware> _logger;
+
+        private static ConcurrentDictionary<string, int> _requestStatitics = new ConcurrentDictionary<string, int>();
+
+        public RequestStatisticsMiddleware(ILogger<RequestStatisticsMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        async Task IMiddleware.InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            _requestStatitics.AddOrUpdate(context.Request.Path, 1, (key, oldValue) => oldValue + 1);
+                        
+            await next.Invoke(context);
+
+            if (_requestStatitics.TryGetValue(context.Request.Path, out var reqNumberAfter))
+            {
+                _logger.LogInformation($"req: {context.Request.Path}, {reqNumberAfter}");
+            }
+        }
+    }
+}
