40fe657e52da7ec7a4e101b082e810ab48748584
Jay Shi <25982097@qq.com>
2024-03-17 07:07:45 +0800

    Add Result and UserRepository

---
+++ Domain/User/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.User.ValueObjects;
+
+namespace Domain.User.Repositories
+{
+    public interface IUserRepository
+    {
+        UserReference Add(Entities.User user);
+        UserReference Update(Entities.User user);
+
+    }
+}
---
+++ Infra.Core/DomainBasics/Result.cs
@@ -0,0 +1,11 @@
+﻿namespace Infra.Core.DomainBasics
+{
+    public class Result<T> where T : class
+    {
+        public T Id { get; set; }
+
+        public string Message { get; set; }
+
+        public bool Success { get; set; }
+    }
+}
