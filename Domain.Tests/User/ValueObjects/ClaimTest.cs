31f99f8e2bcee86c2ee0105e604d74cf96413bc2
Jay Shi <25982097@qq.com>
2024-04-13 13:50:30 +0800

    Add Claim Test

---
+++ Domain.Tests/User/ValueObjects/ClaimTest.cs
@@ -0,0 +1,30 @@
+﻿using Domain.User.ValueObjects;
+using Infra.Core.Test;
+
+namespace Domain.Tests.User.ValueObjects
+{
+    [TestClass]
+    public class ClaimTest
+    {
+        private Claim _claim;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _claim = new Claim(System.Security.Claims.ClaimTypes.Name,string.Empty);
+        }
+
+        [TestMethod, TestCategory(nameof(TestCategoryType.UnitTest))]
+        public void Given_Value_When_SetValue_Then_ValueAssinged()
+        {
+            // Arrange
+            var value = "value";
+
+            // Action
+            _claim.SetValue(value);
+
+            // Assert
+            Assert.AreEqual(value, _claim.Value);
+        }
+    }
+}
