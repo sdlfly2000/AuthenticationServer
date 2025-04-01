0685c76da8dd09724a74dab00d37fb44e6b04dd9
Jay Shi <25982097@qq.com>
2025-04-01 22:56:05 +0800

    docker deployment

--- Deployment/Start.ps1
+++ Deployment/Start.ps1
@@ -5,4 +5,16 @@ Powershell.exe ./Build.ps1
 Powershell.exe ./Deploy.ps1
 
 # Step 3: Restart Service
-./RestartService.ps1
+# ./RestartService.ps1
+
+# Step 3: Clear Existing Image
+./ClearDockerImage.ps1
+
+# Step 4: Build Image
+./BuildDockerImage.ps1
+
+# Step 5: Stop running container
+./StopDockerContainer.ps1
+
+# Step 6: Run container
+./RunDockerImage.ps1
--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM mcr.microsoft.com/dotnet/aspnet:8.0
 WORKDIR /app
-COPY ["Build/AuthService","."]
+COPY [".","."]
 ENTRYPOINT ["dotnet","AuthService.dll","--urls","http://*:4202"]
 
 
