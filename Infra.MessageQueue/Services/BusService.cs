c53e552f1b09d7ec0b67c60968b74fba4ff5222d
Jay Shi <25982097@qq.com>
2025-05-18 16:05:39 +0800

    Log after published message

--- Infra.MessageQueue/Services/BusService.cs
+++ Infra.MessageQueue/Services/BusService.cs
@@ -3,6 +3,7 @@ using Common.Core.DependencyInjection;
 using EasyNetQ;
 using EasyNetQ.Topology;
 using MessageQueue.RabbitMQ.MessageQueue.Marks;
+using Microsoft.Extensions.Logging;
 
 namespace MessageQueue.RabbitMQ.Services
 {
@@ -10,20 +11,23 @@ namespace MessageQueue.RabbitMQ.Services
     public class BusService : IBusService
     {
         private readonly IAdvancedBus _bus;
+        private readonly ILogger<BusService> _logger;
 
-        public BusService(IBus eventbus)
+        public BusService(IBus eventbus, ILogger<BusService> logger)
         {
             _bus = eventbus.Advanced;
+            _logger = logger;
         }
 
         public async Task publish<TMessage>(TMessage message, string routingKey) where TMessage : BaseMessage
         {
-            //var exchangee = await _bus.ExchangeDeclareAsync(message.GetType().Name, ExchangeType.Topic);
             var exchange = new Exchange(message.GetType().Name, ExchangeType.Topic);
 
             var amqpMessage = new Message<TMessage>(message);
 
             await _bus.PublishAsync(exchange, "register", false, amqpMessage);
+
+            _logger.LogInformation($"{nameof(BusService)}: Message published to exchange {{Exchange}} with routing key {{RoutingKey}}, amqpMessageId: {{amqpMessageId}}", exchange.Name, routingKey, amqpMessage.Body.Id);
         }
     }
 }
