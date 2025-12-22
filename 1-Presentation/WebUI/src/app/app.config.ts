c6137a0d232b8007385164a82e16a04d1a7e0b75
Jay Shi <25982097@qq.com>
2025-12-23 00:34:47 +0800

    #22 deploy demo to webui

--- 1-Presentation/WebUI/src/app/app.config.ts
+++ 1-Presentation/WebUI/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
@@ -10,16 +10,14 @@ import { QueryStringService } from '../services/shared.QueryString.service';
 import { StatusMessageService } from '../services/statusmessage.service';
 import { AuthFailureInterceptor } from './auth-failure.interceptor';
 import { AuthInterceptor } from './auth.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      },
-    }),
+    provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
+    provideAnimationsAsync(),
+    providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
     { provide: "BASE_URL", useValue: document.getElementsByTagName('base')[0].href },
     { provide: AuthService },
     { provide: StatusMessageService },
--- 1-Presentation/WebUI/src/app/app.routes.ts
+++ 1-Presentation/WebUI/src/app/app.routes.ts
@@ -1,5 +1,23 @@
 import { Routes } from '@angular/router';
+import { AppLayout } from './layout/component/app.layout';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Documentation } from './pages/documentation/documentation';
+import { Landing } from './pages/landing/landing';
+import { Notfound } from './pages/notfound/notfound';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    component: AppLayout,
+    children: [
+      { path: '', component: Dashboard },
+      { path: 'uikit', loadChildren: () => import('./pages/uikit/uikit.routes') },
+      { path: 'documentation', component: Documentation },
+      { path: 'pages', loadChildren: () => import('./pages/pages.routes') }
+    ]
+  },
+  { path: 'landing', component: Landing },
+  { path: 'notfound', component: Notfound },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.routes') },
+  { path: '**', redirectTo: '/notfound' }
 ];
