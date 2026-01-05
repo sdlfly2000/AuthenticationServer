8a323f85f7b0c9f7b495ad725a4d7b5cc6e8bae0
Jay Shi <25982097@qq.com>
2026-01-05 23:17:24 +0800

    #30 adapt IsFixed in presenation layer

--- 1-Presentation/AuthService/Models/UserClaimModel.cs
+++ 1-Presentation/AuthService/Models/UserClaimModel.cs
@@ -6,5 +6,7 @@ namespace AuthService.Models
     {
         public ClaimTypeValues ClaimType { get; set; }
         public string Value { get; set; }
+
+        public bool IsFixed { get; set; }
     }
 }
